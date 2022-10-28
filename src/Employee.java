b5a49e27d3cda72a99b0aa9c66f6dd7dc5841ff0
Пользователь <woodoo08@mail.ru>
2022-10-29 00:45:37 +0300

    Курсовой проект 1. Повышенная сложность.

--- src/Employee.java
+++ src/Employee.java
@@ -45,16 +45,16 @@ public class Employee {
 
     public static void main(String[] args) {
         Employee[] listEmployee = new Employee[10];
-        listEmployee[0] = new Employee("Иванов Иван Иванович", 1, 45513f);
-        listEmployee[1] = new Employee("Петров Пётр Петрович", 2, 39034f);
-        listEmployee[2] = new Employee("Семёнов Семён Семёнович", 3, 33021f);
-        listEmployee[3] = new Employee("Сидоров Игорь Артёмович", 4, 55123f);
-        listEmployee[4] = new Employee("Смирнов Альберт Генадьевич", 5, 99199f);
-        listEmployee[5] = new Employee("Распутин Владимир Владимирович", 1, 164000.9f);
-        listEmployee[6] = new Employee("Туркин Иван Павлович", 3, 32432.5f);
-        listEmployee[7] = new Employee("Буркова Наталья Ивановна", 4, 112333.9f);
-        listEmployee[8] = new Employee("Михалкова Ольга Петровна", 1, 132000.3f);
-        listEmployee[9] = new Employee("Рубенштерн Антон Григорьевич", 2, 78000.8f);
+        listEmployee[0] = new Employee("Иванов Иван Иванович", 3, 45_513f);
+        listEmployee[1] = new Employee("Петров Пётр Петрович", 5, 39_034f);
+        listEmployee[2] = new Employee("Семёнов Семён Семёнович", 3, 33_021f);
+        listEmployee[3] = new Employee("Сидоров Игорь Артёмович", 4, 75_000f);
+        listEmployee[4] = new Employee("Смирнов Альберт Генадьевич", 5, 99_199f);
+        listEmployee[5] = new Employee("Распутин Владимир Владимирович", 1, 164_000f);
+        listEmployee[6] = new Employee("Туркин Иван Павлович", 4, 32_432f);
+        listEmployee[7] = new Employee("Буркова Наталья Ивановна", 4, 63_333f);
+        listEmployee[8] = new Employee("Михалкова Ольга Петровна", 2, 130_000f);
+        listEmployee[9] = new Employee("Рубенштерн Антон Григорьевич", 2, 100_000f);
 
         System.out.println(listEmployee[4].toString());
         System.out.println(listEmployee[6].toString());
@@ -82,11 +82,151 @@ public class Employee {
         System.out.println("==========================================================================================");
         getAllEmployee(listEmployee);
 
+        getSalaryIndexing(listEmployee);
+
+        System.out.println("==========================================================================================");
+
+        getMinSalaryInDepartment(listEmployee);
+
+        System.out.println("==========================================================================================");
+
+        getMaxSalaryInDepartment(listEmployee);
+
+        System.out.println("==========================================================================================");
+
+        getTotalSalaryInDepartment(listEmployee);
+
+        System.out.println("==========================================================================================");
+
+        getAverageSalaryInDepartment(listEmployee);
+
+        System.out.println("==========================================================================================");
+
+
+        getDepartmentEmployees(listEmployee);
+
+        System.out.println("==========================================================================================");
+
+        getMinSalaryEmployee(listEmployee);
+
+        System.out.println("==========================================================================================");
+
+        getMaxSalaryEmployee(listEmployee);
+
+        System.out.println("==========================================================================================");
+
+        getSalaryIndexingInDepartment(listEmployee);
+
+        System.out.println("==========================================================================================");
 
     }
 
+    private static void getMaxSalaryEmployee(Employee[] listEmployee) {
+        float maxSalaryEmployee = 75_000f;
+        for (Employee item : listEmployee) {
+            if (item.salary >= maxSalaryEmployee) {
+                System.out.println("Зарплата сотрудника " + item.fio + " больше (равна) " + maxSalaryEmployee + "рублей, и составляет " + item.salary + " рублей!");
+            }
+        }
+    }
+
+    private static void getMinSalaryEmployee(Employee[] listEmployee) {
+        float minSalaryEmployee = 56_500f;
+        for (Employee item : listEmployee) {
+            if (item.salary < minSalaryEmployee) {
+                System.out.println("Зарплата сотрудника " + item.fio + " меньше " + minSalaryEmployee + "рублей, и составляет " + item.salary + " рублей!");
+            }
+        }
+    }
+
+    private static void getDepartmentEmployees(Employee[] listEmployee) {
+        int numberDepartment = 4;
+        System.out.println("Отдел №" + numberDepartment);
+        for (Employee item : listEmployee) {
+            if (item.department == numberDepartment) {
+                System.out.println("Сотрудник: " + item.fio + " | Зароботная плата: " + item.salary + " | id: " + item.id);
+            }
+        }
+    }
+
+    private static void getSalaryIndexingInDepartment(Employee[] listEmployee) {
+        int salaryIndexingInDepartment = 10;
+        int numberDepartment = 2;
+        System.out.println("Индексация отдела №" + numberDepartment);
+        for (Employee item : listEmployee) {
+            if (item.department == numberDepartment) {
+                item.salary += item.salary / 100 * salaryIndexingInDepartment;
+                System.out.println("Зарплата сотрудника " + item.fio + " проиндексированна на " + salaryIndexingInDepartment + "% и составляет " + item.salary + " рублей!");
+
+            }
+
+        }
+    }
+
+    private static void getAverageSalaryInDepartment(Employee[] listEmployee) {
+        int numberDepartment = 2;
+        float totalSalaryInDepartment = 0;
+        float averageSalaryInDepartment = 0;
+        int i = 0;
+        for (Employee item : listEmployee) {
+            if (item.department == numberDepartment) {
+                totalSalaryInDepartment = totalSalaryInDepartment + item.salary;
+                i++;
+            }
+            averageSalaryInDepartment = totalSalaryInDepartment / i;
+        }
+        System.out.println("В отделе №" + numberDepartment + " средняя зарплата работника составляет " + averageSalaryInDepartment + " рублей!");
+    }
+
+    private static void getTotalSalaryInDepartment(Employee[] listEmployee) {
+        int numberDepartment = 2;
+        float totalSalaryInDepartment = 0;
+        for (Employee item : listEmployee) {
+            if (item.department == numberDepartment) {
+                totalSalaryInDepartment = totalSalaryInDepartment + item.salary;
+            }
+        }
+        System.out.println("В отделе №" + numberDepartment + " общая сумма зарплат в месяц составляет " + totalSalaryInDepartment + "рублей!");
+    }
+
+
+    private static void getMaxSalaryInDepartment(Employee[] listEmployee) {
+        int numberDepartment = 3;
+        float maxSalaryInDepartment = 0;
+        for (Employee item : listEmployee) {
+            if (item.department == numberDepartment && maxSalaryInDepartment == 0) {
+                maxSalaryInDepartment = item.salary;
+            } else if (item.department == numberDepartment && maxSalaryInDepartment < item.salary) {
+                maxSalaryInDepartment = item.salary;
+            }
+        }
+        System.out.println("В отделе №" + numberDepartment + " максимальная зарплата, по всем сотрудникам, составляет: " + maxSalaryInDepartment);
+    }
+
+
+    private static void getMinSalaryInDepartment(Employee[] listEmployee) {
+        int numberDepartment = 3;
+        float minSalaryInDepartment = 0;
+        for (Employee item : listEmployee) {
+            if (item.department == numberDepartment && minSalaryInDepartment < item.salary) {
+                minSalaryInDepartment = item.salary;
+            } else if (minSalaryInDepartment > item.salary) {
+                minSalaryInDepartment = item.salary;
+            }
+        }
+        System.out.println("В отделе №" + numberDepartment + " минимальная зарплата, по всем сотрудникам, составляет: " + minSalaryInDepartment);
+    }
+
+    private static void getSalaryIndexing(Employee[] listEmployee) {
+        int salaryIndexing = 10;
+        for (Employee item : listEmployee) {
+            item.salary += item.salary / 100 * salaryIndexing;
+            System.out.println("После индексации в " + salaryIndexing + "% " + " зарплата сотрудника " + item.fio + " составила " + item.salary + "рублей");
+        }
+    }
+
     private static void getAverageSalary(Employee[] listEmployee) {
-        float i=0f;
+        int i = 0;
         float averageSalary;
         float totalSalary = 0f;
         for (Employee item : listEmployee) {
@@ -94,7 +234,7 @@ public class Employee {
             i++;
         }
         averageSalary = totalSalary / i;
-        System.out.println("Средняя зарплата по всем отделам: " +averageSalary);
+        System.out.println("Средняя зарплата по всем отделам: " + averageSalary);
     }
 
     private static void getMaxSalary(Employee[] listEmployee) {
